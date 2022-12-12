f5ac0be8684313760a71f7d1bb144c8436dbf3db
amandahiya1 <amandahiya4444@gmail.com>
2022-12-13 02:31:24 +0530

    created app.py

---
+++ api/app.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+# from api.routes import route_user, route_avis, route_login
+# from database.models import Base
+# from database.session import engine
+
+
+def get_application() -> FastAPI:
+    application = FastAPI(title='hedwig')
+
+    origins = ["*"]
+    application.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+    return application
+
+
+# Base.metadata.create_all(bind=engine, checkfirst=True)
+
+app = get_application()
