36a995f69e1a522187c62fcb316c430c081b8fbe
amandahiya1 <amandahiya4444@gmail.com>
2022-12-15 00:04:23 +0530

    created students, teachers, subjects and attendance models.

--- database/models/models.py
+++ database/models/models.py
@@ -1,19 +1,35 @@
 from sqlalchemy import Column, Integer, String, Text, DateTime, ForeignKey, Boolean, Date
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
 
 class Students(Base):
-    pass
+    __tablename__ = "students"
+    s_id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(30), nullable=False)
+    email = Column(String(40), nullable=False, unique=True)
+    password = Column(String(150), nullable=False)
+
 
 class Teachers(Base):
-    pass
+    __tablename__ = "teachers"
+    t_id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(30), nullable=False)
+    email = Column(String(40), nullable=False, unique=True)
+    password = Column(String(150), nullable=False)
+
 
 class Subjects(Base):
-    pass
+    __tablename__ = "subjects"
+    sub_id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(30), nullable=False)
+    t_id = Column(Integer, ForeignKey('teachers.t_id', ondelete="CASCADE"))
+
 
 class Attendance(Base):
-    pass
-
+    __tablename__ = "attendance"
+    a_id = Column(Integer, primary_key=True, index=True)
+    s_id = Column(Integer, ForeignKey('students.s_id', ondelete="CASCADE"))
+    date = Column(Date, nullable=False)
+    attendance = Column(Integer, nullable=False)
