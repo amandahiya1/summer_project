ffdaca3231b637d8c627e7f8a2b8b7099fc50182
amandahiya1 <amandahiya4444@gmail.com>
2022-12-15 23:14:53 +0530

    connection to mysql

---
+++ database/session/session.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+SQLALCHEMY_DATABASE_URL = "mysql://root@localhost/chckattendance"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
