7d90a0f1c63ee5bbfce90a31211a2f74b3511bdb
amandahiya1 <amandahiya4444@gmail.com>
2022-12-15 23:32:18 +0530

    changed title

--- api/app.py
+++ api/app.py
@@ -2,11 +2,11 @@ from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 # from api.routes import route_user, route_avis, route_login
 from database.models.models import Base
-from database.session import engine
+from database.session.session import engine
 
 
 def get_application() -> FastAPI:
-    application = FastAPI(title='hedwig')
+    application = FastAPI(title='Attendance Manager')
 
     origins = ["*"]
     application.add_middleware(
